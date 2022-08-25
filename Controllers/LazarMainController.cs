ae6bad1dfb34528451385ed8050d3e0e107d6606
Lazar Bojanic <93272221+LBojanic50@users.noreply.github.com>
2022-08-25 18:24:03 +0200

    Added movie info to page and trailer

--- Controllers/LazarMainController.cs
+++ Controllers/LazarMainController.cs
@@ -1,15 +1,28 @@
 ﻿using LazarNetMVCApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 using TMDbLib.Client;
+using TMDbLib.Objects.Collections;
+using TMDbLib.Objects.General;
 using TMDbLib.Objects.Movies;
+using TMDbLib.Objects.Search;
 
 namespace LazarNetMVCApp.Controllers {
     public class LazarMainController : Controller {
-        public IActionResult Index() {
-            TMDbClient client = new TMDbClient("923ba34a720eb4f615326820215f419d");
-            Movie apiMovie = client.GetMovieAsync(47964).Result;
-            MovieViewModel movie = new MovieViewModel(0, apiMovie.Id, apiMovie.Title, apiMovie.ReleaseDate, apiMovie.Overview, apiMovie.VoteAverage);
+        TMDbClient client = new TMDbClient("923ba34a720eb4f615326820215f419d");
+        public IActionResult Index() {      
+            Movie apiMovie = searchForExactMovie("Spider-Man");
+            Video apiMovieTrailer = getVideos(apiMovie.Id);
+            MovieViewModel movie = new MovieViewModel(0, apiMovie.Id, apiMovie.Title, apiMovie.PosterPath, apiMovie.ReleaseDate, apiMovie.Overview, apiMovie.VoteAverage, apiMovieTrailer, apiMovieTrailer.Key);
             return View(movie);
+        }
+        public Video getVideos(int movieId) {
+            ResultContainer<Video> videos = client.GetMovieVideosAsync(movieId).Result;
+            return videos.Results[0];
+        }
+        public Movie searchForExactMovie(String movieName) {
+            SearchContainer<SearchMovie> searchMovie = client.SearchMovieAsync(movieName).Result;
+            return client.GetMovieAsync(searchMovie.Results[0].Id).Result;
         }
     }
 }
--- Models/MovieViewModel.cs
+++ Models/MovieViewModel.cs
@@ -1,21 +1,28 @@
-﻿using TMDbLib.Objects.Movies;
+﻿using TMDbLib.Objects.General;
+using TMDbLib.Objects.Movies;
+using static System.Net.WebRequestMethods;
 
 namespace LazarNetMVCApp.Models {
     public class MovieViewModel {
         public int id { get; set; }
         public int movieId { get; set; }
         public String? movieTitle { get; set; }
+        public String? moviePoster { get; set; }
         public DateTime? movieReleaseDate { get; set; }
         public String? movieOverview { get; set; }
         public double movieVoteAverage { get; set; }
-
-        public MovieViewModel(int id, int movieId, string? movieTitle, DateTime? movieReleaseDate, string? movieOverview, double movieVoteAverage) {
+        public Video movieTrailer { get; set; }
+        public String? movieTrailerUrl { get; set; }
+        public MovieViewModel(int id, int movieId, string? movieTitle, string? moviePoster, DateTime? movieReleaseDate, string? movieOverview, double movieVoteAverage, Video movieTrailer, string? movieTrailerUrl) {
             this.id = id;
             this.movieId = movieId;
             this.movieTitle = movieTitle;
+            this.moviePoster = "https://image.tmdb.org/t/p/w500/" + moviePoster;
             this.movieReleaseDate = movieReleaseDate;
             this.movieOverview = movieOverview;
             this.movieVoteAverage = movieVoteAverage;
+            this.movieTrailer = movieTrailer;
+            this.movieTrailerUrl = "https://www.youtube.com/embed/" + movieTrailer.Key;
         }
     }
 }
